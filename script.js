8238ef6805d17d92c321226848bf406d80a91fbb
Sudhanshu Thakur <123307584+Sudhanshu-thakur@users.noreply.github.com>
2023-08-14 20:45:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,10 +75,9 @@ form.addEventListener('submit',(e)=>{
 
 })
 
-// const logo = document.querySelector(".logo")
+ const logo = document.querySelector(".logo")
 
-// logo.addEventListener('click', goToHomePage())
-
-// function goToHomePage (){
-//     window.location = '/';
-// }
+logo.addEventListener('click', goToHomePage())
+ function goToHomePage (){
+    window.location = '/';
+}
